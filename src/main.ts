edfaeb6ef9ef4f6c593b665dab39ef5095955e97
Evgenii Dobrovidov <evgenii.dobrovidov@autodesk.com>
2026-02-06 11:06:37 +0100

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,53 @@
+import "./style.css";
+import { Forma } from "forma-embedded-view-sdk/auto";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <h1>Proposal Test</h1>
+    <div class="card">
+      <button id="updateProposalButton" type="button">Update Current Proposal</button>
+    </div>
+  </div>
+`;
+
+const updateProposalButton = document.querySelector<HTMLButtonElement>(
+  "#updateProposalButton",
+)!;
+updateProposalButton.addEventListener("click", async () => {
+  const proposalId = await Forma.proposal.getId();
+  console.log(`Current Proposal ID: ${proposalId}`);
+
+  const data = await Forma.proposal.get({ proposalId });
+  const terrain = data.children?.find((child) => child.urn.includes("terrain"));
+  if (!terrain) {
+    console.error("Terrain not found");
+    return;
+  }
+
+  const baseKey = Object.keys(data.properties?.flags ?? {})
+    .filter(
+      (key) => (data.properties?.flags?.[key] as { base?: boolean })?.base,
+    )
+    .at(0);
+  if (!baseKey) {
+    console.error("Base key not found in proposal properties");
+    return;
+  }
+
+  const base = data.children?.find((child) => child.key === baseKey);
+  if (!base) {
+    console.error("Base not found");
+    return;
+  }
+
+  await Forma.proposal.update({
+    proposalId,
+    revision: new Date().getTime().toString(),
+    proposal: {
+      name: `${data.properties?.name ?? "MISSING NAME"} - Updated at ${new Date().toISOString()}`,
+      terrain,
+      base,
+      children: data.children ?? [],
+    },
+  });
+});
